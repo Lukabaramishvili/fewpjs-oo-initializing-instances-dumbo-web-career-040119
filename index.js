bd020fe60f8553dc2ac113140bba3e344cfb6f02
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-05-23 15:56:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class Lunch{
 }
 
 class Dinner{
-  constructor(salad, soup, entree,  dessert){
+  constructor(salad, soup, entree, dessert){
   this.salad = salad;
   this.soup = soup;
   this.drink = drink;
